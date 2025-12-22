be7107c9af5bf64816185fef04fade006dc47bdf
旋风SELF <121877340+Self4215@users.noreply.github.com>
2025-12-22 16:14:43 +0800

    添加 Controller 控制器

---
+++ src/main/java/org/self4215/controller/LoginController.java
@@ -0,0 +1,13 @@
+package org.self4215.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+    // 访问 /login 时返回登录页
+    @GetMapping("/login")
+    public String login() {
+        return "login"; // 对应 templates/login.html
+    }
+}
